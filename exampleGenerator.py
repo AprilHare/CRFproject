8913b4ab6610865d8eac907b2d5192dd03839150
luke <luke.rast@gmail.com>
2015-12-12 19:41:41 -0500

    sat evening

--- exampleGenerator.py
+++ exampleGenerator.py
@@ -27,3 +27,57 @@ def hiddenCDF(number):
 def observationCDF(number):
     #outputs a random integer
     return np.random.randint( number - 2, number + 2) 
+
+
+def ribGen():
+    state = 0
+
+    transMat = np.zeros((6, 6))
+    transMat[1, 0] = 0.7
+    transMat[2, 0] = 0.3
+
+    transMat[3, 1] = 1
+    transMat[4, 2] = 1
+
+    transMat[5, 3] = 1
+    transMat[5, 4] = 1
+
+
+    emits = {1: 'r', 2:'r', 3: 'i', 4: 'o', 5:'b' }
+    other = ['r', 'i', 'o', 'b']
+
+    code = {'b': 0, 'i': 1, 'o':2, 'r':3}
+
+    state = 0
+    toEmits = ''
+    actual = ''
+
+    encodedEmits = []
+    encodedActual = []
+
+    while state < 5:
+        nextState = np.random.choice(range(0, 6), p = transMat[:, state])
+        state = nextState
+
+        if np.random.rand() < (28./ 32):
+            toEmits += emits[nextState]
+            encodedEmits.append( code[emits[nextState]] )
+        else:
+            out = np.random.choice(other)
+            print toEmits
+            toEmits += emits[out]
+            encodedEmits.append( code[emits[outs]] )
+
+
+        actual += emits[nextState]
+        encodedActual.append( code[emits[nextState]] ) 
+
+    return (toEmits, actual)#, np.array( [ encodedEmits, encodedActual ] )
+
+train = [ ribGen() for i in range(2000)] 
+test = [ribGen() for i in range(500)]
+
+
+
+
+
